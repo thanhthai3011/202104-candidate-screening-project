a74e9fdeae8aef6b2fad62e0d3bbd0bd873c4926
thanhthai3011 <55083667+thanhthai3011@users.noreply.github.com>
2021-04-30 01:50:51 +0700

    add cpomponent Header, ToDoList, Data.json and config App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import data from "./data.json";
+//components
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState(data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <ToDoList toDoList={toDoList}/>
     </div>
   );
 }
