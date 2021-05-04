41c6652d6de342a6255298ab74d724428adafb31
thanhthai3011 <55083667+thanhthai3011@users.noreply.github.com>
2021-05-04 22:02:18 +0700

    install jest, ESLint, Material UI and config ESLint

--- todo-list/src/Header.js
+++ todo-list/src/Header.js
@@ -1,12 +1,11 @@
-
 import React from 'react';
-
+ 
 const Header = () => {
-    return (
-        <header>
-            <h1>To Do List</h1>
-        </header>
-    );
+   return (
+       <header>
+           <h1>To Do List</h1>
+       </header>
+   );
 };
-
+ 
 export default Header;
